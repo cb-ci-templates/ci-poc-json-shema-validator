07fe6078ac7c0e85dd3b05281498883dcdd01706
cccaternberg <acaternberg@cloudbees.com>
2024-04-29 13:30:15 +0200

    add

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sample hello World
+* https://www.w3schools.io/file/yaml-arrays/ 
+* https://www.jenkins.io/blog/2018/04/25/configuring-jenkins-pipeline-with-yaml-file/
+* https://www.baeldung.com/java-snake-yaml
+
+# Json Scheme validator
+* https://json-schema.org/implementations#validators-command-line
+* https://hub.docker.com/r/miy4/json-schema-validator 
+* https://www.npmjs.com/package/pajv
+* https://github.com/python-jsonschema/jsonschema 
+
+## ajv-cli
+* https://hub.docker.com/u/3scale
+* https://ajv.js.org/packages/ajv-cli.html
+* https://github.com/3scale-archive/docker-ajv
+* https://www.npmjs.com/package/ajv-cli
+
+```
+docker run  -v $(PWD):/data 3scale/ajv validate -s ci-json-scheme.json -d ci.yaml
+```
---
+++ testYamlConfig.groovy
@@ -0,0 +1,65 @@
+@Grab(group='org.yaml', module='snakeyaml', version='1.29')
+
+import org.yaml.snakeyaml.Yaml
+import java.nio.file.Files
+import java.nio.file.Paths
+
+def myMap = [:]
+
+// Read YAML file
+def yamlText = new String(Files.readAllBytes(Paths.get("./ci.yaml")))
+
+// Parse YAML
+def configYaml = new Yaml().load(yamlText)
+println configYaml.ciTemplate
+
+println configYaml.qs.sonar.enabled
+println configYaml.qs.sonar.host
+println configYaml.qs.sonar.credentialID
+
+println configYaml.qs.checkmarx.enabled
+println configYaml.qs.checkmarx.host
+println configYaml.qs.checkmarx.credentialID
+
+println configYaml.build.steps.get(0)
+println configYaml.build.steps.get(1)
+
+println "iterate build environment"
+configYaml.build.environment.each { env ->
+       println env
+}
+println "iterate build steps"
+configYaml.build.steps.collect { step ->
+       println step
+}
+
+println configYaml.kaniko.dockerHost
+println configYaml.kaniko.credentialID
+
+println configYaml.deploy.type
+println configYaml.deploy.cluster
+println configYaml.deploy.image
+
+
+
+
+//println(myMap)
+// Access properties
+
+//println("Age: ${yaml.age}")
+
+/*
+if ('appName' in this) {
+    println "Is a PTC instance ${appName}"
+}else{
+    def appName="sample-app-helloworld"
+    println "Is a Markerfile instance ${appName}"
+}
+if ('k8AgentPodyaml' in this) {
+    println "Is a PTC instance ${k8AgentPodyaml}"
+}else{
+    def k8AgentPodyaml="podTemplate-curl.yaml"
+    println "Is a Markerfile instance ${k8AgentPodyaml}"
+}
+*/
+
